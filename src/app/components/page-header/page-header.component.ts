4cfdafe039369523d03d00675fcefd178e1fba72
m.krsmanovic <m.krsmanovic@fuentis.com>
2023-04-05 18:14:32 +0200

    positions

--- src/app/components/page-header/page-header.component.ts
+++ src/app/components/page-header/page-header.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-page-header',
   template: `
-    <div class="pl-4">
+    <div class="pl-4 p-2">
       <h1>{{ pageTitle }}</h1>
     </div>
   `,
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -14,20 +14,38 @@ import { AuthorizationService } from 'src/app/services/authorization.service';
         text="Please verify your email address"
         styleClass="mr-2 w-full"
       ></p-message>
-
-      <h3>{{ user.photoURL }} {{ user?.displayName }}</h3>
-      <p>{{ user?.email }}</p>
-      <button
-        icon="pi pi-sign-out"
-        (click)="logOut()"
-        class=""
-        pButton
-        type="button"
-        label="Sign Out"
-      ></button>
+      <div
+        class="flex flex-column justify-content-evenly align-items-center wrap"
+      >
+        <div>
+          <h1>{{ user.photoURL }} {{ user?.displayName }}</h1>
+          <div class="flex align-items-center">
+            <i
+              class="pi pi-envelope text-primary p-1"
+              style="font-size: 1rem"
+            ></i>
+            <p>{{ user?.email }}</p>
+          </div>
+        </div>
+        <div></div>
+        <button
+          icon="pi pi-sign-out"
+          (click)="logOut()"
+          class="w-full mt-4"
+          pButton
+          type="button"
+          label="Sign Out"
+        ></button>
+      </div>
     </div>
   `,
-  styles: [],
+  styles: [
+    `
+      .wrap {
+        height: calc(100vh - 120px);
+      }
+    `,
+  ],
 })
 export class ProfileComponent {
   constructor(private auth: AuthorizationService) {}
